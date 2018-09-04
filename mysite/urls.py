0909601488879476a998e6fefeb2b5b501d03340
Prakash Kumar <pkg98875@gmail.com>
2018-09-04 03:52:09 -0700

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url('',include('blog.urls')),
+    path(r'^admin/', admin.site.urls),
+    path('',include('blog.urls')),
 ]
